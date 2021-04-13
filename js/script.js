d5e2b40ea470953f4b7f8eedb2ca7bea792881dc
Samuelton <samueltonkelvi02@gmail.com>
2021-04-12 21:37:25 -0300

    feat: card list init

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+const URL_API = "https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1";
+
+const products = [];
+
+// Connect API Service
+// function Connect(){
+
+//     let request = new XMLHttpRequest();
+//     request.open('GET', URL_API);
+//     request.responseType = 'json';
+//     request.send();
+
+//     request.onload = function() {
+//         let data = request.response;
+//         products.push(data);
+//     }
+
+// }
